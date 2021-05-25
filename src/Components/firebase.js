49c83010220e29ce32550885e11ef694bcff9d76
Kaushal-Panchal <krdpanchal@gmail.com>
2021-05-25 15:27:54 +0530

    Hide API Key

--- src/Components/firebase.js
+++ src/Components/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyC_YW-uLtL_RXG2Yuj8le2tNj0loDOIQF0",
-    authDomain: "multiplayer-chess-301b6.firebaseapp.com",
-    projectId: "multiplayer-chess-301b6",
-    storageBucket: "multiplayer-chess-301b6.appspot.com",
-    messagingSenderId: "788667624867",
-    appId: "1:788667624867:web:e86d596e605ae07d15be16"
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: "chess-2ecc5.firebaseapp.com",
+    projectId: "chess-2ecc5",
+    storageBucket: "chess-2ecc5.appspot.com",
+    messagingSenderId: "733439324702",
+    appId: "1:733439324702:web:0855067db2192060e7740f"
   };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
